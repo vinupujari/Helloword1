2e1928ae68bdc4340f80cec8a52357bb8b9dc60b
vinayak <pujarivinu99@gmail.com>
2019-08-02 14:53:06 +0530

    added helloword program

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+		for(int i=0;i<10;i++){
+			
+        System.out.println("Hello, World"); 
+		}
+    } 
+} 
